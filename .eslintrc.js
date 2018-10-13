108da8e52db923fd02e2f8cf07934d744127d489
Thiago Coelho <thiag0coelho@outlook.com>
2018-10-13 18:22:36 -0300

    Adding lint to accept .js extension

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     "extends": "airbnb",
     rules: {
-        "linebreak-style": 0
+        "linebreak-style": 0,
+        "react/jsx-filename-extension": [1, {"extensions": [".js", ".jsx"]}]
     }
 };
